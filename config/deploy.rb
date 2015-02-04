193384c60daba69dd362bf0e542c8c202c47b4b9
Eric Roos <ericroos13@gmail.com>
2015-02-03 18:10:00 -0600

    update deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,15 +10,15 @@ require 'mina/rvm'    # for rvm support. (http://rvm.io)
 #   repository   - Git repo to clone from. (needed by mina/git)
 #   branch       - Branch name to deploy. (needed by mina/git)
 
-set :domain, '130.211.143.43'
+set :domain, 'eric_roos@130.211.143.43'
 set :deploy_to, '/var/www/rails_apps/shop_finder'
-set :repository, 'git://...'
+set :repository, 'git@github.com:EricRoos/shop-finder.git'
 set :branch, 'master'
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
 set :shared_paths, ['config/database.yml', 'log']
-
+set :rvm_path, "/usr/local/rvm/bin/rvm"
 # Optional settings:
 #   set :user, 'foobar'    # Username in the server to SSH to.
 #   set :port, '30000'     # SSH port number.
@@ -31,7 +31,7 @@ task :environment do
   # invoke :'rbenv:load'
 
   # For those using RVM, use this to load an RVM version@gemset.
-  invoke :'rvm:use[ruby-1.9.3-p125@default]'
+  invoke :'rvm:use[ruby 2.1.1p76@default]'
 end
 
 # Put any custom mkdir's in here for when `mina setup` is ran.
